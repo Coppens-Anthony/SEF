0545d5551fabcba5392834f656d9c41b0e16bdc2
Coppens-Anthony <145543157+Coppens-Anthony@users.noreply.github.com>
2024-07-26 13:59:27 +0200

    Initial commit

---
+++ wp-content/themes/sef/src/views/stores.php
@@ -0,0 +1,311 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="author" content="Anthony Coppens">
+    <meta name="keywords" content="accueil, ouverture, famille, dimension humaine et relationnelle, inscription du don, &eacute;conomie circulaire, &eacute;cologie, asbl, sans-abris, projets, h&eacute;bergements">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" href="/wp-content/themes/sef/public/css/style.css">
+    <title>Projets</title>
+</head>
+<body>
+<h1 class="sr-only">Nos projets</h1>
+<header>
+    <nav id="mainNav">
+        <a href="/">
+            <svg id="Calque_2" data-name="Calque 2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 68.83 114.78">
+                <defs>
+                    <style>
+                        .cls-1 {
+                            font-size: 44.66px;
+                        }
+
+                        .cls-1, .cls-2, .cls-3 {
+                            fill: #3253a1;
+                        }
+
+                        .cls-4 {
+                            stroke-width: 2px;
+                        }
+
+                        .cls-4, .cls-5 {
+                            fill: #fff;
+                            stroke: #3253a1;
+                            stroke-miterlimit: 10;
+                        }
+
+                        .cls-2 {
+                            font-size: 33.98px;
+                        }
+
+                        .cls-3 {
+                            font-size: 31.03px;
+                        }
+                    </style>
+                </defs>
+                <g id="Calque_1-2" data-name="Calque 1">
+                    <rect class="cls-4" x="2.07" y="56.02" width="64.32" height="56.12" rx=".38" ry=".38"/>
+                    <text class="cls-1" transform="translate(0 49.52) scale(1.15 1)"><tspan x="0" y="0">s</tspan></text>
+                    <text class="cls-3" transform="translate(24.39 25.97) scale(1.15 1)"><tspan x="0" y="0">E</tspan></text>
+                    <text class="cls-2" transform="translate(49.82 49.96) scale(1.15 1)"><tspan x="0" y="0">F</tspan></text>
+                    <path class="cls-4" d="m34.27,21.52L2.46,55.79c-.09.09-.01.23.13.23h63.63c.14,0,.21-.14.13-.23l-31.82-34.27c-.07-.07-.19-.07-.26,0Z"/>
+                    <path class="cls-5" d="m28.12,96.29h12.55v16.32h-12.35c-.11,0-.2-.09-.2-.2v-16.12h0Z"/>
+                    <path class="cls-5" d="m35.73,114.17l-7.61-1.57v-15.58c0-.38.4-.66.83-.57l6.78,1.4v16.32Z"/>
+                    <g>
+                        <rect class="cls-5" x="40.85" y="65.33" width="14.41" height="15.59" transform="translate(96.11 146.25) rotate(180)"/>
+                        <rect class="cls-5" x="13.2" y="65.33" width="14.41" height="15.59" transform="translate(40.8 146.25) rotate(180)"/>
+                    </g>
+                </g>
+            </svg>
+        </a>
+        <h2 class="sr-only">Navigation Principale</h2>
+        <a href="donate.php" id="donateMobile" title="Vers la page de soutien">SOUTENIR</a>
+        <label for="burgerMenu">☰</label>
+        <input type="checkbox" id="burgerMenu" name="burgerMenu">
+        <nav id="burgerNav">
+            <h3 class="sr-only">Burger Menu Navigation</h3>
+            <ul>
+                <li>
+                    <a href="/" title="Vers la page SEF">SEF</a>
+                </li>
+                <li>
+                    <a href="projects.php" title="Vers la page des projets">Projets</a>
+                </li>
+                <li>
+                    <a href="stores.php" class="main" title="Vers la page des magasins">Magasins</a>
+                </li>
+                <li>
+                    <a href="accommodation.php" title="Vers la page des h&eacute;bergements">H&eacute;bergements</a>
+                </li>
+                <li class="navContact">
+                    <a href="contact.php" title="Vers la page de contact">Contact</a>
+                </li>
+            </ul>
+        </nav>
+    </nav>
+    <nav class="onlyScreen">
+        <a href="/">
+            <svg id="Calque_2" data-name="Calque 2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 68.83 114.78">
+                <defs>
+                    <style>
+                        .cls-1 {
+                            font-size: 44.66px;
+                        }
+
+                        .cls-1, .cls-2, .cls-3 {
+                            fill: #3253a1;
+                        }
+
+                        .cls-4 {
+                            stroke-width: 2px;
+                        }
+
+                        .cls-4, .cls-5 {
+                            fill: #fff;
+                            stroke: #3253a1;
+                            stroke-miterlimit: 10;
+                        }
+
+                        .cls-2 {
+                            font-size: 33.98px;
+                        }
+
+                        .cls-3 {
+                            font-size: 31.03px;
+                        }
+                    </style>
+                </defs>
+                <g id="Calque_1-2" data-name="Calque 1">
+                    <rect class="cls-4" x="2.07" y="56.02" width="64.32" height="56.12" rx=".38" ry=".38"/>
+                    <text class="cls-1" transform="translate(0 49.52) scale(1.15 1)"><tspan x="0" y="0">s</tspan></text>
+                    <text class="cls-3" transform="translate(24.39 25.97) scale(1.15 1)"><tspan x="0" y="0">E</tspan></text>
+                    <text class="cls-2" transform="translate(49.82 49.96) scale(1.15 1)"><tspan x="0" y="0">F</tspan></text>
+                    <path class="cls-4" d="m34.27,21.52L2.46,55.79c-.09.09-.01.23.13.23h63.63c.14,0,.21-.14.13-.23l-31.82-34.27c-.07-.07-.19-.07-.26,0Z"/>
+                    <path class="cls-5" d="m28.12,96.29h12.55v16.32h-12.35c-.11,0-.2-.09-.2-.2v-16.12h0Z"/>
+                    <path class="cls-5" d="m35.73,114.17l-7.61-1.57v-15.58c0-.38.4-.66.83-.57l6.78,1.4v16.32Z"/>
+                    <g>
+                        <rect class="cls-5" x="40.85" y="65.33" width="14.41" height="15.59" transform="translate(96.11 146.25) rotate(180)"/>
+                        <rect class="cls-5" x="13.2" y="65.33" width="14.41" height="15.59" transform="translate(40.8 146.25) rotate(180)"/>
+                    </g>
+                </g>
+            </svg>
+        </a>
+        <h2 class="sr-only">Navigation pour oridnateur</h2>
+        <ul>
+            <li>
+                <a href="/" title="Vers la page SEF">SEF</a>
+            </li>
+            <li>
+                <a href="projects.php" title="Vers la page des projets">Projets</a>
+            </li>
+            <li>
+                <a href="stores.php" class="main" title="Vers la page des magasins">Magasins</a>
+            </li>
+            <li>
+                <a href="accommodation.php" title="Vers la page des h&eacute;bergements">H&eacute;bergements</a>
+            </li>
+            <li class="navContact">
+                <a href="contact.php" title="Vers la page de contact">Contact</a>
+            </li>
+            <li id="donateScreen">
+                <a href="donate.php" title="Vers la page de soutien">Soutenir</a>
+            </li>
+        </ul>
+    </nav>
+</header>
+<main>
+    <section class="intro_stores">
+        <h2 class="sr-only">Les magasins</h2>
+        <p>&Agrave; travers nos magasins solidaires, nous offrons &agrave; nos b&eacute;n&eacute;ficiaires des produits essentiels &agrave; des prix abordables tout en finan&ccedil;ant nos activit&eacute;s.
+            Ces espaces favorisent l&apos;insertion sociale et professionnelle en offrant des opportunit&eacute;s de rencontre et d&apos;&eacute;change. En participant &agrave; leur gestion,
+            nos b&eacute;n&eacute;ficiaires acqui&egrave;rent des comp&eacute;tences pr&eacute;cieuses, retrouvant ainsi dignit&eacute; et autonomie. Vos dons et achats contribuent &agrave; bâtir une communaut&eacute; plus solidaire et inclusive.</p>
+        <img id="clothe" alt="Image repr&eacute;sentant un v&ecirc;tement" src="../img/stores/clothe.jpg" height="206" width="206"/>
+        <img id="book" alt="Image repr&eacute;sentant un livre" src="../img/stores/book.jpg" height="206" width="206"/>
+        <img id="furniture" alt="Image repr&eacute;sentant un meuble" src="../img/stores/furniture.jpg" height="206" width="206"/>
+    </section>
+    <section class="sections" id="stores">
+        <h2>Nos Magasins</h2>
+        <div>
+            <article>
+                <h3>LE BRIC &Agrave; BRAC</h3>
+                <div>
+                    <img alt="Image du magasin" src="../img/stores/bric_a_brac.jpg" height="212" width="320"/>
+                    <p>Rue du March&eacute;, 35 - 4500 Huy 085/21.57.52</p>
+                </div>
+            </article>
+            <article>
+                <h3>LA BOUQUINERIE
+                    DU P’TIT AUGUSTIN</h3>
+                <div>
+                    <img alt="Image du magasin" src="../img/stores/bouquinerie.jpg" height="212" width="320"/>
+                    <p>Rue des Augustins, 27 - 4500 Huy 085/21.12.56</p>
+                </div>
+            </article>
+            <article>
+                <h3>LE D&Eacute;P&Ocirc;T-MEUBLES</h3>
+                <div>
+                    <img alt="Image du magasin" src="../img/stores/depot_meuble.jpg" height="212" width="320"/>
+                    <p>Ch&eacute;e de Waremme, 18 - 4500 Huy 0496/03.70.90</p>
+                </div>
+            </article>
+        </div>
+    </section>
+    <section class="sections" id="table">
+        <h2>Les horaires</h2>
+        <table>
+            <caption class="sr-only">Horaires d&apos;ouverture des magasins</caption>
+            <tr>
+                <th>&nbsp;</th>
+                <th>Le BRIC &Agrave; BRAC</th>
+                <th>LA BOUQUINERIE</th>
+                <th>LE D&Eacute;P&Ocirc;T-MEUBLES</th>
+            </tr>
+            <tr>
+                <th class="day">Lundi</th>
+                <td>FERM&Eacute;</td>
+                <td>FERM&Eacute;</td>
+                <td>FERM&Eacute;</td>
+            </tr>
+            <tr>
+                <th class="day">Mardi</th>
+                <td>14h - 17h</td>
+                <td>12h - 17h</td>
+                <td>9h - 17h</td>
+            </tr>
+            <tr>
+                <th class="day">Mercredi</th>
+                <td>9h - 17h</td>
+                <td>12h - 17h</td>
+                <td>9h - 17h</td>
+            </tr>
+            <tr>
+                <th class="day">Jeudi</th>
+                <td>9h - 12h</td>
+                <td>12h - 17h</td>
+                <td>FERM&Eacute;</td>
+            </tr>
+            <tr>
+                <th class="day">Vendredi</th>
+                <td>14h - 17h</td>
+                <td>12h - 17h</td>
+                <td>FERM&Eacute;</td>
+            </tr>
+            <tr>
+                <th class="day">Samedi</th>
+                <td>FERM&Eacute;</td>
+                <td>10h - 13h <sup class="red infoSup">*</sup></td>
+                <td>10h - 16H</td>
+            </tr>
+            <tr>
+                <th class="day">Dimanche</th>
+                <td>FERM&Eacute;</td>
+                <td>FERM&Eacute;</td>
+                <td>FERM&Eacute;</td>
+            </tr>
+        </table>
+        <small class="red">*&nbsp;:&nbsp;uniquement le premier samedi du mois</small>
+    </section>
+</main>
+<footer>
+    <section class="footer-information">
+        <h2 class="sr-only">Footer Information</h2>
+        <article id="footerNav">
+            <h3>Navigation</h3>
+            <ul>
+                <li>
+                    <a href="/" title="Vers la page SEF">SEF</a>
+                </li>
+                <li>
+                    <a href="/wp-content/themes/sef/src/views/projects.php" title="Vers la page des projets">Projets</a>
+                </li>
+                <li>
+                    <a href="/wp-content/themes/sef/src/views/stores.php" title="Vers la page des magasins">Magasins</a>
+                </li>
+                <li>
+                    <a href="/wp-content/themes/sef/src/views/accommodation.php" title="Vers la page des h&eacute;bergements">H&eacute;bergements</a>
+                </li>
+                <li>
+                    <a href="/wp-content/themes/sef/src/views/contact.php" title="Vers la page de contact">Contact</a>
+                </li>
+                <li>
+                    <a href="/wp-content/themes/sef/src/views/donate.php" title="Vers la page de soutien">Soutenir</a>
+                </li>
+            </ul>
+        </article>
+        <article id="footerCoords">
+            <h3>Coordonn&eacute;es</h3>
+            <ul itemscope itemtype="https://schema.org/Organization">
+                <li itemprop="telephone">
+                    085 / 21 57 52
+                </li>
+                <li itemprop="email">
+                    contact@sefasbl.com
+                </li>
+                <li itemprop="address">
+                    Rue du March&eacute;, 30
+                </li>
+                <li itemprop="address">
+                    4500, Huy
+                </li>
+                <li>
+                    G&eacute;n&eacute;ral&nbsp;:&nbsp;BE34 0000 2750 8590
+                </li>
+                <li>
+                    Sp&eacute;cifique&nbsp;:&nbsp;BE43 2400 2934 2201
+                </li>
+            </ul>
+        </article>
+    </section>
+    <section class="map">
+        <h2 class="sr-only">Carte int&eacute;ractive</h2>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d2536.86158016455!2d5.2410361758847275!3d50.51814228248832!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNTDCsDMxJzA1LjMiTiA1wrAxNCczNy4wIkU!5e0!3m2!1sfr!2sbe!4v1718539613216!5m2!1sfr!2sbe"
+                width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </section>
+    <small>
+        Retrouvez nous sur <a href="https://www.facebook.com/sef.huy?locale=fr_FR" title="Vers notre compte Facebook">Facebook</a>
+    </small>
+</footer>
+</body>
+</html>
