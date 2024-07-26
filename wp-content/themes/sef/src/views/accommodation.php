2c3bad8b774e918e4aa4ff9eaa22264fb1c2d17f
Coppens-Anthony <145543157+Coppens-Anthony@users.noreply.github.com>
2024-07-26 16:10:47 +0200

    Redone the store page, the accommodation page, the contact page and the donate page

--- wp-content/themes/sef/src/views/accommodation.php
+++ wp-content/themes/sef/src/views/accommodation.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="author" content="Anthony Coppens">
-    <meta name="keywords" content="accueil, ouverture, famille, dimension humaine et relationnelle, inscription du don, &eacute;conomie circulaire, &eacute;cologie, asbl, sans-abris, projets, h&eacute;bergements">
+    <meta name="keywords"
+          content="accueil, ouverture, famille, dimension humaine et relationnelle, inscription du don, &eacute;conomie circulaire, &eacute;cologie, asbl, sans-abris, projets, h&eacute;bergements">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <link rel="stylesheet" href="/wp-content/themes/sef/public/css/style.css">
@@ -46,15 +47,24 @@
                 </defs>
                 <g id="Calque_1-2" data-name="Calque 1">
                     <rect class="cls-4" x="2.07" y="56.02" width="64.32" height="56.12" rx=".38" ry=".38"/>
-                    <text class="cls-1" transform="translate(0 49.52) scale(1.15 1)"><tspan x="0" y="0">s</tspan></text>
-                    <text class="cls-3" transform="translate(24.39 25.97) scale(1.15 1)"><tspan x="0" y="0">E</tspan></text>
-                    <text class="cls-2" transform="translate(49.82 49.96) scale(1.15 1)"><tspan x="0" y="0">F</tspan></text>
-                    <path class="cls-4" d="m34.27,21.52L2.46,55.79c-.09.09-.01.23.13.23h63.63c.14,0,.21-.14.13-.23l-31.82-34.27c-.07-.07-.19-.07-.26,0Z"/>
+                    <text class="cls-1" transform="translate(0 49.52) scale(1.15 1)">
+                        <tspan x="0" y="0">s</tspan>
+                    </text>
+                    <text class="cls-3" transform="translate(24.39 25.97) scale(1.15 1)">
+                        <tspan x="0" y="0">E</tspan>
+                    </text>
+                    <text class="cls-2" transform="translate(49.82 49.96) scale(1.15 1)">
+                        <tspan x="0" y="0">F</tspan>
+                    </text>
+                    <path class="cls-4"
+                          d="m34.27,21.52L2.46,55.79c-.09.09-.01.23.13.23h63.63c.14,0,.21-.14.13-.23l-31.82-34.27c-.07-.07-.19-.07-.26,0Z"/>
                     <path class="cls-5" d="m28.12,96.29h12.55v16.32h-12.35c-.11,0-.2-.09-.2-.2v-16.12h0Z"/>
                     <path class="cls-5" d="m35.73,114.17l-7.61-1.57v-15.58c0-.38.4-.66.83-.57l6.78,1.4v16.32Z"/>
                     <g>
-                        <rect class="cls-5" x="40.85" y="65.33" width="14.41" height="15.59" transform="translate(96.11 146.25) rotate(180)"/>
-                        <rect class="cls-5" x="13.2" y="65.33" width="14.41" height="15.59" transform="translate(40.8 146.25) rotate(180)"/>
+                        <rect class="cls-5" x="40.85" y="65.33" width="14.41" height="15.59"
+                              transform="translate(96.11 146.25) rotate(180)"/>
+                        <rect class="cls-5" x="13.2" y="65.33" width="14.41" height="15.59"
+                              transform="translate(40.8 146.25) rotate(180)"/>
                     </g>
                 </g>
             </svg>
@@ -118,15 +128,24 @@
                 </defs>
                 <g id="Calque_1-2" data-name="Calque 1">
                     <rect class="cls-4" x="2.07" y="56.02" width="64.32" height="56.12" rx=".38" ry=".38"/>
-                    <text class="cls-1" transform="translate(0 49.52) scale(1.15 1)"><tspan x="0" y="0">s</tspan></text>
-                    <text class="cls-3" transform="translate(24.39 25.97) scale(1.15 1)"><tspan x="0" y="0">E</tspan></text>
-                    <text class="cls-2" transform="translate(49.82 49.96) scale(1.15 1)"><tspan x="0" y="0">F</tspan></text>
-                    <path class="cls-4" d="m34.27,21.52L2.46,55.79c-.09.09-.01.23.13.23h63.63c.14,0,.21-.14.13-.23l-31.82-34.27c-.07-.07-.19-.07-.26,0Z"/>
+                    <text class="cls-1" transform="translate(0 49.52) scale(1.15 1)">
+                        <tspan x="0" y="0">s</tspan>
+                    </text>
+                    <text class="cls-3" transform="translate(24.39 25.97) scale(1.15 1)">
+                        <tspan x="0" y="0">E</tspan>
+                    </text>
+                    <text class="cls-2" transform="translate(49.82 49.96) scale(1.15 1)">
+                        <tspan x="0" y="0">F</tspan>
+                    </text>
+                    <path class="cls-4"
+                          d="m34.27,21.52L2.46,55.79c-.09.09-.01.23.13.23h63.63c.14,0,.21-.14.13-.23l-31.82-34.27c-.07-.07-.19-.07-.26,0Z"/>
                     <path class="cls-5" d="m28.12,96.29h12.55v16.32h-12.35c-.11,0-.2-.09-.2-.2v-16.12h0Z"/>
                     <path class="cls-5" d="m35.73,114.17l-7.61-1.57v-15.58c0-.38.4-.66.83-.57l6.78,1.4v16.32Z"/>
                     <g>
-                        <rect class="cls-5" x="40.85" y="65.33" width="14.41" height="15.59" transform="translate(96.11 146.25) rotate(180)"/>
-                        <rect class="cls-5" x="13.2" y="65.33" width="14.41" height="15.59" transform="translate(40.8 146.25) rotate(180)"/>
+                        <rect class="cls-5" x="40.85" y="65.33" width="14.41" height="15.59"
+                              transform="translate(96.11 146.25) rotate(180)"/>
+                        <rect class="cls-5" x="13.2" y="65.33" width="14.41" height="15.59"
+                              transform="translate(40.8 146.25) rotate(180)"/>
                     </g>
                 </g>
             </svg>
@@ -158,34 +177,58 @@
     <section class="intro">
         <h2 class="sr-only">Les h&eacute;bergements</h2>
         <p>L&apos;insertion ou la r&eacute;insertion sociale d&apos;adultes et de familles en difficult&eacute;
-            Le temps pass&eacute; au SEF doit être mis &agrave; profit par chacun pour r&eacute;gler ses questions administratives,
-            repenser sa fa&ccedil;on de vivre avec les autres et pr&eacute;parer son d&eacute;part. Afin, si possible, de ne pas revenir.</p>
+            Le temps pass&eacute; au SEF doit être mis &agrave; profit par chacun pour r&eacute;gler ses questions
+            administratives,
+            repenser sa fa&ccedil;on de vivre avec les autres et pr&eacute;parer son d&eacute;part. Afin, si possible,
+            de ne pas revenir.</p>
         <img alt="Image repr&eacute;sentant les projets" src="../img/accommodation.jpg" height="500" width="1000"/>
     </section>
     <section class="sections" id="houses">
         <h2>Nos h&eacute;bergements</h2>
         <article>
             <h3 class="sr-only">Premier h&eacute;bergement</h3>
-            <p>
-                <i>Cette maison se situe Rue des Augustins, 27 &agrave; Huy.</i>
-                <br>
-                <br>
-                Elle nous permet d&apos;h&eacute;berger en moyenne <span class="blue">40 personnes</span> par an qui sont dans le besoin.
-                Elle dispose de <span class="blue">8 lits</span> en h&eacute;bergement d&apos;urgence ainsi qu&apos;un logement supervis&eacute; (1 &agrave; 2 personnes),
-                ce qui permet une autonomie pendant quelque temps dans une structure semi-communautaire.
-            </p>
+            <div>
+                <p class="houses-address">
+                    Rue des Augustins, 27 &agrave; Huy.
+                </p>
+                <p class="houses-description">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus amet architecto blanditiis dolor
+                    dolores.
+                </p>
+                <ul>
+                    <li>
+                        8 lits
+                    </li>
+                    <li>
+                        40 personnes par an
+                    </li>
+                </ul>
+            </div>
+
             <img alt="Image du premier h&eacute;bergement" src="../img/houses/firstHouse.jpg" height="427" width="320"/>
         </article>
-        <article>
+        <article class="secondHouse">
             <h3 class="sr-only">Deuxi&egrave;me h&eacute;bergement</h3>
-            <img alt="Image du deuxi&egrave;me h&eacute;bergement" src="../img/houses/secondHouse.jpg" height="600" width="450"/>
-            <p class="secondHouse">
-                <i>Cette maison se situe Rue du March&eacute; 35 &agrave; Huy.</i>
-                <br>
-                <br>
-                Elle nous permet d&apos;h&eacute;berger en moyenne <span class="blue">40 personnes</span> par an qui sont dans le besoin.
-                Elle dispose de <span class="blue">8 lits</span> en h&eacute;bergement d&apos;urgence.
-            </p>
+            <img alt="Image du deuxi&egrave;me h&eacute;bergement" src="../img/houses/secondHouse.jpg" height="600"
+                 width="450"/>
+            <div>
+                <p class="houses-address">
+                    Rue du March&eacute; 35 &agrave; Huy.
+                </p>
+                <p class="houses-description">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus amet architecto blanditiis dolor
+                    dolores.
+                </p>
+                <ul>
+                    <li>
+                        8 lits
+                    </li>
+                    <li>
+                        40 personnes par an
+                    </li>
+                </ul>
+            </div>
+
         </article>
     </section>
 </main>
@@ -205,7 +248,8 @@
                     <a href="/wp-content/themes/sef/src/views/stores.php" title="Vers la page des magasins">Magasins</a>
                 </li>
                 <li>
-                    <a href="/wp-content/themes/sef/src/views/accommodation.php" title="Vers la page des h&eacute;bergements">H&eacute;bergements</a>
+                    <a href="/wp-content/themes/sef/src/views/accommodation.php"
+                       title="Vers la page des h&eacute;bergements">H&eacute;bergements</a>
                 </li>
                 <li>
                     <a href="/wp-content/themes/sef/src/views/contact.php" title="Vers la page de contact">Contact</a>
@@ -238,12 +282,6 @@
                 </li>
             </ul>
         </article>
-    </section>
-    <section class="map">
-        <h2 class="sr-only">Carte int&eacute;ractive</h2>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d2536.86158016455!2d5.2410361758847275!3d50.51814228248832!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNTDCsDMxJzA1LjMiTiA1wrAxNCczNy4wIkU!5e0!3m2!1sfr!2sbe!4v1718539613216!5m2!1sfr!2sbe"
-                width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
-                referrerpolicy="no-referrer-when-downgrade"></iframe>
     </section>
     <small>
         Retrouvez nous sur <a href="https://www.facebook.com/sef.huy?locale=fr_FR" title="Vers notre compte Facebook">Facebook</a>
--- wp-content/themes/sef/src/views/donate.php
+++ wp-content/themes/sef/src/views/donate.php
@@ -185,7 +185,7 @@
         </article>
     </section>
     <section class="sections">
-        <h2>Les dons</h2>
+        <h2>Les diff&eacute;rents types de dons</h2>
         <article class="donateCategory">
             <h3 class="sr-only">Les moyens de donation</h3>
             <div>
@@ -276,12 +276,6 @@
                 </li>
             </ul>
         </article>
-    </section>
-    <section class="map">
-        <h2 class="sr-only">Carte int&eacute;ractive</h2>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d2536.86158016455!2d5.2410361758847275!3d50.51814228248832!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNTDCsDMxJzA1LjMiTiA1wrAxNCczNy4wIkU!5e0!3m2!1sfr!2sbe!4v1718539613216!5m2!1sfr!2sbe"
-                width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
-                referrerpolicy="no-referrer-when-downgrade"></iframe>
     </section>
     <small>
         Retrouvez nous sur <a href="https://www.facebook.com/sef.huy?locale=fr_FR" title="Vers notre compte Facebook">Facebook</a>
