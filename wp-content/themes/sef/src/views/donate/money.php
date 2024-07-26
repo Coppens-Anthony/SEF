a1eed69f5f6218b9104441530e87d7edb6f2e706
Coppens-Anthony <145543157+Coppens-Anthony@users.noreply.github.com>
2024-07-26 16:10:47 +0200

    Redone the store page, the accommodation page, the contact page and the donate page

--- wp-content/themes/sef/src/views/donate/money.php
+++ wp-content/themes/sef/src/views/donate/money.php
@@ -276,12 +276,6 @@
             </ul>
         </article>
     </section>
-    <section class="map">
-        <h2 class="sr-only">Carte int&eacute;ractive</h2>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d2536.86158016455!2d5.2410361758847275!3d50.51814228248832!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNTDCsDMxJzA1LjMiTiA1wrAxNCczNy4wIkU!5e0!3m2!1sfr!2sbe!4v1718539613216!5m2!1sfr!2sbe"
-                width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
-                referrerpolicy="no-referrer-when-downgrade"></iframe>
-    </section>
     <small>
         Retrouvez nous sur <a href="https://www.facebook.com/sef.huy?locale=fr_FR" title="Vers notre compte Facebook">Facebook</a>
     </small>
--- wp-content/themes/sef/src/views/stores.php
+++ wp-content/themes/sef/src/views/stores.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="author" content="Anthony Coppens">
-    <meta name="keywords" content="accueil, ouverture, famille, dimension humaine et relationnelle, inscription du don, &eacute;conomie circulaire, &eacute;cologie, asbl, sans-abris, projets, h&eacute;bergements">
+    <meta name="keywords"
+          content="accueil, ouverture, famille, dimension humaine et relationnelle, inscription du don, &eacute;conomie circulaire, &eacute;cologie, asbl, sans-abris, projets, h&eacute;bergements">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <link rel="stylesheet" href="/wp-content/themes/sef/public/css/style.css">
@@ -46,15 +47,24 @@
                 </defs>
                 <g id="Calque_1-2" data-name="Calque 1">
                     <rect class="cls-4" x="2.07" y="56.02" width="64.32" height="56.12" rx=".38" ry=".38"/>
-                    <text class="cls-1" transform="translate(0 49.52) scale(1.15 1)"><tspan x="0" y="0">s</tspan></text>
-                    <text class="cls-3" transform="translate(24.39 25.97) scale(1.15 1)"><tspan x="0" y="0">E</tspan></text>
-                    <text class="cls-2" transform="translate(49.82 49.96) scale(1.15 1)"><tspan x="0" y="0">F</tspan></text>
-                    <path class="cls-4" d="m34.27,21.52L2.46,55.79c-.09.09-.01.23.13.23h63.63c.14,0,.21-.14.13-.23l-31.82-34.27c-.07-.07-.19-.07-.26,0Z"/>
+                    <text class="cls-1" transform="translate(0 49.52) scale(1.15 1)">
+                        <tspan x="0" y="0">s</tspan>
+                    </text>
+                    <text class="cls-3" transform="translate(24.39 25.97) scale(1.15 1)">
+                        <tspan x="0" y="0">E</tspan>
+                    </text>
+                    <text class="cls-2" transform="translate(49.82 49.96) scale(1.15 1)">
+                        <tspan x="0" y="0">F</tspan>
+                    </text>
+                    <path class="cls-4"
+                          d="m34.27,21.52L2.46,55.79c-.09.09-.01.23.13.23h63.63c.14,0,.21-.14.13-.23l-31.82-34.27c-.07-.07-.19-.07-.26,0Z"/>
                     <path class="cls-5" d="m28.12,96.29h12.55v16.32h-12.35c-.11,0-.2-.09-.2-.2v-16.12h0Z"/>
                     <path class="cls-5" d="m35.73,114.17l-7.61-1.57v-15.58c0-.38.4-.66.83-.57l6.78,1.4v16.32Z"/>
                     <g>
-                        <rect class="cls-5" x="40.85" y="65.33" width="14.41" height="15.59" transform="translate(96.11 146.25) rotate(180)"/>
-                        <rect class="cls-5" x="13.2" y="65.33" width="14.41" height="15.59" transform="translate(40.8 146.25) rotate(180)"/>
+                        <rect class="cls-5" x="40.85" y="65.33" width="14.41" height="15.59"
+                              transform="translate(96.11 146.25) rotate(180)"/>
+                        <rect class="cls-5" x="13.2" y="65.33" width="14.41" height="15.59"
+                              transform="translate(40.8 146.25) rotate(180)"/>
                     </g>
                 </g>
             </svg>
@@ -118,15 +128,24 @@
                 </defs>
                 <g id="Calque_1-2" data-name="Calque 1">
                     <rect class="cls-4" x="2.07" y="56.02" width="64.32" height="56.12" rx=".38" ry=".38"/>
-                    <text class="cls-1" transform="translate(0 49.52) scale(1.15 1)"><tspan x="0" y="0">s</tspan></text>
-                    <text class="cls-3" transform="translate(24.39 25.97) scale(1.15 1)"><tspan x="0" y="0">E</tspan></text>
-                    <text class="cls-2" transform="translate(49.82 49.96) scale(1.15 1)"><tspan x="0" y="0">F</tspan></text>
-                    <path class="cls-4" d="m34.27,21.52L2.46,55.79c-.09.09-.01.23.13.23h63.63c.14,0,.21-.14.13-.23l-31.82-34.27c-.07-.07-.19-.07-.26,0Z"/>
+                    <text class="cls-1" transform="translate(0 49.52) scale(1.15 1)">
+                        <tspan x="0" y="0">s</tspan>
+                    </text>
+                    <text class="cls-3" transform="translate(24.39 25.97) scale(1.15 1)">
+                        <tspan x="0" y="0">E</tspan>
+                    </text>
+                    <text class="cls-2" transform="translate(49.82 49.96) scale(1.15 1)">
+                        <tspan x="0" y="0">F</tspan>
+                    </text>
+                    <path class="cls-4"
+                          d="m34.27,21.52L2.46,55.79c-.09.09-.01.23.13.23h63.63c.14,0,.21-.14.13-.23l-31.82-34.27c-.07-.07-.19-.07-.26,0Z"/>
                     <path class="cls-5" d="m28.12,96.29h12.55v16.32h-12.35c-.11,0-.2-.09-.2-.2v-16.12h0Z"/>
                     <path class="cls-5" d="m35.73,114.17l-7.61-1.57v-15.58c0-.38.4-.66.83-.57l6.78,1.4v16.32Z"/>
                     <g>
-                        <rect class="cls-5" x="40.85" y="65.33" width="14.41" height="15.59" transform="translate(96.11 146.25) rotate(180)"/>
-                        <rect class="cls-5" x="13.2" y="65.33" width="14.41" height="15.59" transform="translate(40.8 146.25) rotate(180)"/>
+                        <rect class="cls-5" x="40.85" y="65.33" width="14.41" height="15.59"
+                              transform="translate(96.11 146.25) rotate(180)"/>
+                        <rect class="cls-5" x="13.2" y="65.33" width="14.41" height="15.59"
+                              transform="translate(40.8 146.25) rotate(180)"/>
                     </g>
                 </g>
             </svg>
@@ -157,37 +176,42 @@
 <main>
     <section class="intro_stores">
         <h2 class="sr-only">Les magasins</h2>
-        <p>&Agrave; travers nos magasins solidaires, nous offrons &agrave; nos b&eacute;n&eacute;ficiaires des produits essentiels &agrave; des prix abordables tout en finan&ccedil;ant nos activit&eacute;s.
-            Ces espaces favorisent l&apos;insertion sociale et professionnelle en offrant des opportunit&eacute;s de rencontre et d&apos;&eacute;change. En participant &agrave; leur gestion,
-            nos b&eacute;n&eacute;ficiaires acqui&egrave;rent des comp&eacute;tences pr&eacute;cieuses, retrouvant ainsi dignit&eacute; et autonomie. Vos dons et achats contribuent &agrave; bâtir une communaut&eacute; plus solidaire et inclusive.</p>
-        <img id="clothe" alt="Image repr&eacute;sentant un v&ecirc;tement" src="../img/stores/clothe.jpg" height="206" width="206"/>
+        <p>&Agrave; travers nos magasins solidaires, nous offrons &agrave; nos b&eacute;n&eacute;ficiaires des produits
+            essentiels &agrave; des prix abordables tout en finan&ccedil;ant nos activit&eacute;s.
+            Ces espaces favorisent l&apos;insertion sociale et professionnelle en offrant des opportunit&eacute;s de
+            rencontre et d&apos;&eacute;change. En participant &agrave; leur gestion,
+            nos b&eacute;n&eacute;ficiaires acqui&egrave;rent des comp&eacute;tences pr&eacute;cieuses, retrouvant ainsi
+            dignit&eacute; et autonomie. Vos dons et achats contribuent &agrave; bâtir une communaut&eacute; plus
+            solidaire et inclusive.</p>
+        <img id="clothe" alt="Image repr&eacute;sentant un v&ecirc;tement" src="../img/stores/clothe.jpg" height="206"
+             width="206"/>
         <img id="book" alt="Image repr&eacute;sentant un livre" src="../img/stores/book.jpg" height="206" width="206"/>
-        <img id="furniture" alt="Image repr&eacute;sentant un meuble" src="../img/stores/furniture.jpg" height="206" width="206"/>
+        <img id="furniture" alt="Image repr&eacute;sentant un meuble" src="../img/stores/furniture.jpg" height="206"
+             width="206"/>
     </section>
     <section class="sections" id="stores">
         <h2>Nos Magasins</h2>
         <div>
             <article>
+                <img alt="Image du magasin" src="../img/stores/bric_a_brac.jpg" height="212" width="320"/>
                 <h3>LE BRIC &Agrave; BRAC</h3>
-                <div>
-                    <img alt="Image du magasin" src="../img/stores/bric_a_brac.jpg" height="212" width="320"/>
-                    <p>Rue du March&eacute;, 35 - 4500 Huy 085/21.57.52</p>
-                </div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquam animi aut debitis deleniti
+                    doloremque esse, eveniet id illum.</p>
+                <p class="store-address">Rue du March&eacute;, 35 - 4500 Huy 085/21.57.52</p>
             </article>
             <article>
-                <h3>LA BOUQUINERIE
-                    DU P’TIT AUGUSTIN</h3>
-                <div>
-                    <img alt="Image du magasin" src="../img/stores/bouquinerie.jpg" height="212" width="320"/>
-                    <p>Rue des Augustins, 27 - 4500 Huy 085/21.12.56</p>
-                </div>
+                <img alt="Image du magasin" src="../img/stores/bouquinerie.jpg" height="212" width="320"/>
+                <h3>LA BOUQUINERIE</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquam animi aut debitis deleniti
+                    doloremque esse, eveniet id illum.</p>
+                <p class="store-address">Rue des Augustins, 27 - 4500 Huy 085/21.12.56</p>
             </article>
             <article>
+                <img alt="Image du magasin" src="../img/stores/depot_meuble.jpg" height="212" width="320"/>
                 <h3>LE D&Eacute;P&Ocirc;T-MEUBLES</h3>
-                <div>
-                    <img alt="Image du magasin" src="../img/stores/depot_meuble.jpg" height="212" width="320"/>
-                    <p>Ch&eacute;e de Waremme, 18 - 4500 Huy 0496/03.70.90</p>
-                </div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid amet debitis doloremque dolorum
+                    ducimus et eveniet.</p>
+                <p class="store-address">Ch&eacute;e de Waremme, 18 - 4500 Huy 0496/03.70.90</p>
             </article>
         </div>
     </section>
@@ -263,7 +287,8 @@
                     <a href="/wp-content/themes/sef/src/views/stores.php" title="Vers la page des magasins">Magasins</a>
                 </li>
                 <li>
-                    <a href="/wp-content/themes/sef/src/views/accommodation.php" title="Vers la page des h&eacute;bergements">H&eacute;bergements</a>
+                    <a href="/wp-content/themes/sef/src/views/accommodation.php"
+                       title="Vers la page des h&eacute;bergements">H&eacute;bergements</a>
                 </li>
                 <li>
                     <a href="/wp-content/themes/sef/src/views/contact.php" title="Vers la page de contact">Contact</a>
@@ -296,12 +321,6 @@
                 </li>
             </ul>
         </article>
-    </section>
-    <section class="map">
-        <h2 class="sr-only">Carte int&eacute;ractive</h2>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d2536.86158016455!2d5.2410361758847275!3d50.51814228248832!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNTDCsDMxJzA1LjMiTiA1wrAxNCczNy4wIkU!5e0!3m2!1sfr!2sbe!4v1718539613216!5m2!1sfr!2sbe"
-                width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
-                referrerpolicy="no-referrer-when-downgrade"></iframe>
     </section>
     <small>
         Retrouvez nous sur <a href="https://www.facebook.com/sef.huy?locale=fr_FR" title="Vers notre compte Facebook">Facebook</a>
