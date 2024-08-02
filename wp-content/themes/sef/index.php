15f2926ced03ce0e68fa1447f207814802bcaa3e
Coppens-Anthony <145543157+Coppens-Anthony@users.noreply.github.com>
2024-08-02 15:51:13 +0200

    ACF wordpress

--- wp-content/themes/sef/index.php
+++ wp-content/themes/sef/index.php
@@ -2,21 +2,20 @@
 /*
 Template Name: Home Page
 */
+
+$text_intro = get_field('text_intro');
+$first_section_title = get_field('first_section_title');
+$second_section_title = get_field('second_section_title');
+
 include 'header.php'; ?>
 <main>
     <section class="intro">
         <h2 class="sr-only">Introduction</h2>
-        <p>
-            <b>L&apos;insertion ou la r&eacute;insertion sociale d&apos;adultes et de familles en difficult&eacute;.</b>
-            Le temps pass&eacute; au SEF doit &ecirc;tre mis &agrave; profit par chacun pour r&eacute;gler ses questions
-            administratives, repenser
-            sa fa&ccedil;on de vivre avec les autres et pr&eacute;parer son d&eacute;part. Afin, si possible, de ne pas
-            revenir&hellip;
-        </p>
+            <?= $text_intro ?>
         <img src="/wp-content/themes/sef/src/img/accueil.jpg" height="322" width="564" alt="Image contextuelle"/>
     </section>
     <section id="actualities" class="sections">
-        <h2>Nos r&eacute;centes actualit&eacute;s</h2>
+        <h2><?= $first_section_title ?></h2>
         <ul>
             <li id="mezzanine">
                 <article>
@@ -55,15 +54,15 @@ include 'header.php'; ?>
             Vous changez des vies</h2>
         <ul>
             <li class="transition-button">
-                <a href="/wp-content/themes/sef/src/views/donate.php" title="Vers la page de soutien">Soutenir</a>
+                <a href="/soutenir/" title="Vers la page de soutien">Soutenir</a>
             </li>
             <li class="transition-button">
-                <a href="/wp-content/themes/sef/src/views/contact.php" title="Vers la page de contact">Contact</a>
+                <a href="/contact/" title="Vers la page de contact">Contact</a>
             </li>
         </ul>
     </section>
     <section class="sections" id="faq">
-        <h2>Retrouvez ici les réponses à vos questions les plus fréquentes.</h2>
+        <h2><?= $second_section_title ?></h2>
         <div class="faq-container">
             <div>
                 <label for="1">Comment puis-je devenir b&eacute;n&eacute;vole&nbsp;?</label>
